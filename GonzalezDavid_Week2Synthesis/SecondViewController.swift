3a2017ef27f137d1ad2bb8825cc2a48dc60d30d7
David Gonzalez Jr <davidgonzalezjr@yahoo.com>
2019-01-09 14:18:18 -0500

    Entering this project in for the first time

---
+++ GonzalezDavid_Week2Synthesis/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  GonzalezDavid_Week2Synthesis
+//
+//  Created by David Gonzalez Jr on 12/5/17.
+//  Copyright © 2017 David Gonzalez Jr. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var continentName: UILabel!
+    @IBOutlet weak var continentInfo: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GonzalezDavid_Week2Synthesis/ViewController.swift
+++ GonzalezDavid_Week2Synthesis/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    var countryTotal: Int = 0
+    var populationTotal: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        totalLabel.text = "There are \(countryTotal) countries in the world with a total population of \(populationTotal)."
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func goToSecond(sender: UIButton) {
+        let tag = sender.tag
+        switch tag{
+        case 0:
+            
+        case 1:
+            
+        case 2:
+            
+        case 3:
+            
+        case 4:
+            
+        case 5:
+            
+        default:
+            print("Either Case 2 was tapped, or something really bad happened!")
+        }
+        performSegue(withIdentifier: "toSecondViewController", sender: sender)
+    }
+    
 }
 
