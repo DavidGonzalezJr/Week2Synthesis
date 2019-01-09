e3ca96a89d6648198bdd3841a409ba0d382b589b
David Gonzalez Jr <davidgonzalezjr@yahoo.com>
2019-01-09 18:29:31 -0500

    Adding the save segue for the third vc

--- GonzalezDavid_Week2Synthesis/SecondViewController.swift
+++ GonzalezDavid_Week2Synthesis/SecondViewController.swift
@@ -25,7 +25,9 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func saveCountryPop(segue: UIStoryboardSegue) {
+    }
+    
     /*
     // MARK: - Navigation
 
